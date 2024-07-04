4f7eb1f500b340a8bdcb2737c3a5ab8328570219
joshkim2441 <joshkim2441@gmail.com>
2024-07-04 21:18:44 +0300

    Corrections

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,11 +8,11 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    ]
+        ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,6 +3,6 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Sums a list of floats."""
     return sum(input_list)
